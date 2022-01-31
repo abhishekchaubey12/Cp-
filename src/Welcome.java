488900b71416d596ac09d36ebdabe97633ee75b6
Abhishek Chaubey <abhishek.chaubey_cs19@gla.ac.in>
2022-01-31 21:04:27 +0530

    Second Commit

---
+++ src/Welcome.java
@@ -0,0 +1,13 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Abhishek Kumar Chaubey
+ *   Date: 31/01/2022
+ *   Time: 21:02
+ *   File: Welcome.java
+ */
+
+public class Welcome {
+    public static void main(String[] args) {
+        System.out.println("Hey welcome to cp++ repo.");
+        System.out.println("Thank You!");
+    }
+}
