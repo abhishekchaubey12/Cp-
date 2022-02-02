a5ea26ec5bc7808de457f3f22d0aa103feb0ff20
Abhishek Chaubey <abhishek.chaubey_cs19@gla.ac.in>
2022-02-02 18:03:13 +0530

    Some cp question

---
+++ src/some_Question/CeilingNumber.java
@@ -0,0 +1,11 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Abhishek Kumar Chaubey
+ *   Date: 02/02/2022
+ *   Time: 18:02
+ *   File: CeilingNumber.java
+ */
+
+package some_Question;
+
+public class CeilingNumber {
+}
