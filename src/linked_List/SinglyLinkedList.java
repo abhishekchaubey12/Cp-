14af135fb62ec96fdeb3558d22c411e09b7a06be
Abhishek Chaubey <abhishek.chaubey_cs19@gla.ac.in>
2022-02-05 11:52:36 +0530

    SLL

---
+++ src/linked_List/SinglyLinkedList.java
@@ -0,0 +1,11 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Abhishek Kumar Chaubey
+ *   Date: 05/02/2022
+ *   Time: 11:51
+ *   File: SinglyLinkedList.java
+ */
+
+package linked_List;
+
+public class SinglyLinkedList {
+}
