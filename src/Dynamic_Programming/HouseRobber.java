98da590ca238d5e58c4221bb7431cde9f2c9b5a7
Abhishek Chaubey <abhishekchaubey.ab@gmail.com>
2022-08-23 22:27:38 +0530

    House Robber

--- src/Dynamic_Programming/HouseRobber.java
+++ src/Dynamic_Programming/HouseRobber.java
@@ -7,6 +7,32 @@
 
 package Dynamic_Programming;
 
-public class HouseRobber {
+import java.util.HashMap;
 
+public class HouseRobber {
+    public static int rob(int[] nums) {
+
+        return maxMoney(nums, 0, new HashMap<Integer, Integer>());
+    }
+
+    public static int maxMoney(int[] nums, int currentIndex, HashMap<Integer, Integer> memo) {
+        if (currentIndex >= nums.length) {
+            return 0;
+        }
+        int currentKey = currentIndex;=
+        if (memo.containsKey(currentIndex)) {
+            return memo.get(currentKey);
+        }
+        int rob = nums[currentIndex] + maxMoney(nums, currentIndex + 2, memo);
+        int noRob = maxMoney(nums, currentIndex + 1, memo);
+
+        memo.put(currentKey, Math.max(rob, noRob));
+
+        return Math.max(rob, noRob);
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println(rob(new int[]{2, 5, 3, 9, 5, 3}));
+    }
 }
